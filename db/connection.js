ca8468219e9d6d1d53ce6aa6579e01512e96ebfe
venn <vennboddula@gmail.com>
2021-05-30 15:51:59 +0530

    Fourth commit

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,13 @@
 const mongoose=require('mongoose')
 const uri='mongodb+srv://venn:venn123@cluster0.ziajh.mongodb.net/test?retryWrites=true&w=majority'
 async function connectDB(){
-await mongoose.connect(uri)
+await mongoose.connect(
+    uri,
+    {
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }
+    )
 console.log("CONNECTED")
 }
 module.exports=connectDB;
