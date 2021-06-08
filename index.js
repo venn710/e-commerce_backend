76dcde6558ec540f2e9bbcc7ddc1c65b626d7857
venn <vennboddula@gmail.com>
2021-06-08 12:39:25 +0530

    Cart added

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express=require('express')
 const mongoose=require('mongoose')
 require('dotenv').config({path:"config.env"})
 const product_data=require('./user_schema/product')
+const cart_data=require('./user_schema/cart_Prods')
 const app=express()
-const port=process.env.PORT
+// const port=process.env.PORT
+port =5000
 mongoose.connect(
-    process.env.mongo_url,
+    // process.env.mongo_url,
+    'mongodb+srv://venn:venn123@cluster0.ziajh.mongodb.net/test?retryWrites=true&w=majority',
     {
         useNewUrlParser:true,
         useUnifiedTopology:true,
         useFindAndModify: false,
     }
     ).then(co => console.log('connected to mongodb..')).catch(e=>console.log('could not connect to mongodb', e))
-
 app.get('/',(req,res)=>{
     console.log("HIIIIIIIIIII")
     res.status(200).send("Hello world...!!")
@@ -52,7 +54,7 @@ app.get('/all',async function(req,res)
         res.status(404).send("ERROR");
     }
 })
-app.get('/:category/:type',async(req,res)=>
+app.get('products/:category/:type',async(req,res)=>
 {
     const category=req.params.category
     const typed=req.params.type
@@ -66,7 +68,7 @@ app.get('/:category/:type',async(req,res)=>
         res.status(404).send("ERROR");
         else
         {
-        res.json(data);
+        res.json(data)
         }
     })}
     catch(err)
@@ -74,8 +76,71 @@ app.get('/:category/:type',async(req,res)=>
         res.status(404).send("ERROR");
     }
 })
+app.get('/cart/:usermail',async(req,res)=>{
+    const mail=req.params.usermail
+    console.log(mail)
+    try{
+        cart_data.find({}).where({'usermail':mail}).exec((err,data)=>
+        {
+            if(err)
+            res.status(404).send(err)
+            else
+            res.json(data)
+        })
+    }
+    catch(err)
+    {
+        res.status(404).send(err)
+    }
+})
 app.get("*", (req, res) => {
-    console.log("HIIIIIIIIIIIIIIIIIII")
     res.status(404).send("oops cant find");
   });
+app.use(express.json(
+    {
+        limit:'100mb',
+    }
+));
+app.use(express.urlencoded({
+    extended: true,
+    urlencoded:true,
+    limit:'100mb',
+  }));
+app.post('/',function(req,res)
+{
+    // console.log(req.body);
+    var new_prod= new product_data(req.body)
+    new_prod.save(function(err,data)
+    {
+        if(err)
+        {
+            console.log(err)
+            res.status(200).send("An Error Occured")
+        }
+        else{
+        console.log("SUCCESSFULLY INSERTED")
+        res.status(200).send("POsted")
+        }
+    })
+})
+app.post('/cart',function(req,res)
+{
+    var cart= new cart_data(req.body)
+    // print(req.body)
+    cart.save(
+        function(err,data)
+        {
+            if(err)
+            {
+                console.log("ERROr")
+                res.status(200).send("An Error Ocuured")
+        }
+        else
+        {
+            console.log("SUCCESSFULLY INSERTED")
+            res.status(200).send("POsted")
+        }
+    }
+    )
+})
   app.listen(port,()=>console.log("Startedddddddddddd"))
--- user_schema/product.js
+++ user_schema/product.js
@@ -3,6 +3,7 @@ let schema=mongoose.Schema({
     brand:String,
     id:String,
     description:String,
+    image:String,
     size:Number,
     title:String,
     price:Number
