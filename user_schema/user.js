fec716520abb94f3b9a96a0cc7af90f4bac29223
venn <vennboddula@gmail.com>
2021-08-01 18:32:50 +0530

    addded orders

--- user_schema/user.js
+++ user_schema/user.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose')
 let schema=mongoose.Schema({
     id:String,
     email:String,
-    pass:String
+    pass:String,
+    adress:Map
 })
 module.exports=mongoose.model('users',schema)
